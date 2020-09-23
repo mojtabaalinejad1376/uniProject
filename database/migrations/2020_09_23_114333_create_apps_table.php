b95c65a5ee391c580cbbfee0e0f361ccdd9907af
mojtaba.hosseinalinejad <mojtaba.hosseinalinejad@psg.network>
2020-09-23 15:34:52 +0330

    Initial Commit

---
+++ database/migrations/2020_09_23_114333_create_apps_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateAppsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('apps', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->integer('price');
+            $table->string('volume');
+            $table->string('version');
+            $table->string('uploader');
+            $table->string('description');
+            $table->string('file');
+            $table->boolean('status')->default(false);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('apps');
+    }
+}
